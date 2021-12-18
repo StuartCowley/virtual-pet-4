a8a445e0f31537b59aa7eae10030cc0cb21b2ca6
Chris Peakin <christopher.peakin@googlemail.com>
2021-12-18 13:17:40 +0000

    added fitness and hunger

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ At this stage, the read-me will be very bare, but I will update in due time as I
 From 16th December onwards, I will try to provide updates done on that day. Once I've finished, a more thorough read-me will be in place and I may move this to a journal-type file, but these updates should provide a better description of what's been done. One reason for this is the commit messages when committing to GitHub have to be rather brief.
 
 Updates 16Dec2021: Previously, the pet object was empty. Now it contains a name, an age and the growUp function, which ages the pet 1 year when called. A few tests have also been written, asking whether the pet is an object, and whether that object has a name and age and whether the growUp function actually increases the age.
+
+Updates 18Dec2021: Added fitness and hunger levels to the pet function, which also changes as a result of growUp(). Parameters are tested.
--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -12,12 +12,43 @@ describe('constructor', () => {
       const pet = new Pet('Fido');
       expect(pet.age).toEqual(0);
     });
+    it('has initial hunger of 0', () =>{
+      const pet = new Pet('Fido');
+      expect(pet.hunger).toEqual(0);
+    });
+    it('has initial fitness of 10', () => {
+      const pet = new Pet('Fido');
+      expect(pet.fitness).toEqual(10);
+    });
   });
   
   describe('growUp', () => {
-    it('increments the age by 1', () =>{
+    it('increments Fidos age by 1', () =>{
       const pet = new Pet('Fido');
       pet.growUp();
       expect(pet.age).toEqual(1);
     });
+    it('increments Fidos age by 3 if we call the function 3 times', () =>{
+      const pet = new Pet('Fido)');
+      pet.growUp();
+      pet.growUp();
+      pet.growUp();
+      expect(pet.age).toEqual(3);
+    })
   });
+
+  describe('hunger', ()=> {
+    it('increments Fidos hunger by 5 on aging by 1', () =>{
+      const fido = new Pet('Fido');
+      fido.growUp();
+      expect(fido.hunger).toEqual(5);
+    })
+  })
+
+  describe('fitness', () => {
+    it('decreases Fidos fitness by 3 on aging by 1', () =>{
+      const fido = new Pet('Fido');
+      fido.growUp();
+      expect(fido.fitness).toEqual(7);
+    })
+  })
